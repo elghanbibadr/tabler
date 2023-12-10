b101704057e63d4f689206cdb6a79c9537b907e6
badr ghanbi <bghanbi50@gmail.com>
2023-12-10 17:47:13 +0100

    more refactoring

--- index.js
+++ index.js
@@ -1,2 +1,82 @@
-import { supabase } from "./supabase/Supabase";
-console.log(supabase);
+// import { createClient } from "@supabase/supabase-js";
+
+// // Your Supabase URL and API key
+// const supabaseUrl = "https://kunglsicsakuulhxguaa.supabase.co";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1bmdsc2ljc2FrdXVsaHhndWFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE1NjUwMzcsImV4cCI6MjAxNzE0MTAzN30.6mrpRpWqouSp1YUpQpu1gvNZCtFkKXEh4B68OZdKo6Q";
+
+// // Create a Supabase client
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// // Now you can use the 'supabase' client in your code
+// console.log(supabase);
+
+// console.log("hi");
+// userstable.innerHTML = `  <table class="table table-vcenter">
+// <thead>
+//   <tr>
+//     <th>User</th>
+//     <th>Commit</th>
+//     <th>Date</th>
+//   </tr>
+// </thead>
+// <tbody>
+//   <tr>
+//     <td class="w-1">
+//       <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
+//     </td>
+//     <td class="td-truncate">
+//       <div class="text-truncate">
+//         Fix dart Sass compatibility (#29755)
+//       </div>
+//     </td>
+//     <td class="text-nowrap text-muted">28 Nov 2019</td>
+//   </tr>
+//   <tr>
+//     <td class="w-1">
+//       <span class="avatar avatar-sm">JL</span>
+//     </td>
+//     <td class="td-truncate">
+//       <div class="text-truncate">
+//         Change deprecated html tags to text decoration classes (#29604)
+//       </div>
+//     </td>
+//     <td class="text-nowrap text-muted">27 Nov 2019</td>
+//   </tr>
+//   <tr>
+//     <td class="w-1">
+//       <span class="avatar avatar-sm" style="background-image: url(./static/avatars/002m.jpg)"></span>
+//     </td>
+//     <td class="td-truncate">
+//       <div class="text-truncate">
+//         justify-content:between ⇒ justify-content:space-between (#29734)
+//       </div>
+//     </td>
+//     <td class="text-nowrap text-muted">26 Nov 2019</td>
+//   </tr>
+//   <tr>
+//     <td class="w-1">
+//       <span class="avatar avatar-sm" style="background-image: url(./static/avatars/003m.jpg)"></span>
+//     </td>
+//     <td class="td-truncate">
+//       <div class="text-truncate">
+//         Update change-version.js (#29736)
+//       </div>
+//     </td>
+//     <td class="text-nowrap text-muted">26 Nov 2019</td>
+//   </tr>
+//   <tr>
+//     <td class="w-1">
+//       <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000f.jpg)"></span>
+//     </td>
+//     <td class="td-truncate">
+//       <div class="text-truncate">
+//         Regenerate package-lock.json (#29730)
+//       </div>
+//     </td>
+//     <td class="text-nowrap text-muted">25 Nov 2019</td>
+//   </tr>
+// </tbody>
+// </table>
+
+// `;
